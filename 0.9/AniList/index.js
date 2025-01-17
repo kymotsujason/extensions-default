3874d5e808bfe52f6e03071ac63f9f8d34cb6ad5
kymotsujason <kymotsujason@gmail.com>
2025-01-17 16:55:34 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@28016fd18bdcbdb7ce91693587be5189ad451444 🚀

--- 0.9/AniList/index.js
+++ 0.9/AniList/index.js
@@ -3260,7 +3260,7 @@ query($id: Int) {
       );
       const mangaDetails = json.data.Media;
       throw new Error(
-        `Not implemented manga progress ${JSON.stringify(sourceMangaInfo)}`
+        `Not implemented manga progress ${JSON.stringify(mangaDetails)}`
       );
     }
     async getMangaProgressManagementForm(mangaId) {
