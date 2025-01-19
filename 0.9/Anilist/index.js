f70abbf32c73882cfbe55d8eef54b621c65f7687
kymotsujason <kymotsujason@gmail.com>
2025-01-19 12:50:15 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@43ae9a09fa6807d3c1ad5543c96eda235b850635 🚀

--- 0.9/Anilist/index.js
+++ 0.9/Anilist/index.js
@@ -2923,9 +2923,6 @@ query($id: Int) {
     Application.setState(userInfo, "userInfo");
   }
   var SettingsForm = class extends import_types.Form {
-    get requiresExplicitSubmission() {
-      return true;
-    }
     getSections() {
       return [
         (0, import_types.Section)("oAuthSection", [
@@ -3075,6 +3072,9 @@ query($id: Int) {
         rating: "0"
       };
       this.anilistManga = anilistManga;
+    }
+    get requiresExplicitSubmission() {
+      return true;
     }
     getSections() {
       return [
