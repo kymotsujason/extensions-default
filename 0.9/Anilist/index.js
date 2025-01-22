55a1c0354f9242451790f99b16a45d3dcee0f121
kymotsujason <kymotsujason@gmail.com>
2025-01-22 10:57:49 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@2532a83959766f58a396ecf7a82e80a0a33672ef 🚀

--- 0.9/Anilist/index.js
+++ 0.9/Anilist/index.js
@@ -3706,6 +3706,7 @@ query($id: Int) {
       return this.getUserInfo() != void 0;
     }
     async refreshUserInfo() {
+      throw new Error(`Not implemented ${this.getAccessToken()}`);
       const accessToken = this.getAccessToken();
       if (accessToken == void 0) {
         return Application.setState(void 0, "userInfo");
