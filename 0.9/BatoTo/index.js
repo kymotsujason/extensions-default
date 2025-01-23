3841688fcd1720a10464ccef7c6bce4a95e3733d
kymotsujason <kymotsujason@gmail.com>
2025-01-23 05:56:33 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@912d28c5641658c98f7a61e73d68595023ae6dde 🚀

--- 0.9/BatoTo/index.js
+++ 0.9/BatoTo/index.js
@@ -25170,7 +25170,7 @@ Type: ${row["type"]}`
     const pages = imgList.map(
       (value, index2) => `${value}?${tknList[index2]}`
     );
-    console.log(pages);
+    throw new Error(JSON.stringify(pages));
     const chapterDetails = App.createChapterDetails({
       id: chapterId,
       mangaId,
