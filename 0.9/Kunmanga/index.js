ece90ab109b70406d8b154da68a10255e7bc4b81
kymotsujason <kymotsujason@gmail.com>
2025-02-12 12:05:27 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@2fe772f8352c56ad37bd74e47dca6410ea35fb99 🚀

--- 0.9/Kunmanga/index.js
+++ 0.9/Kunmanga/index.js
@@ -16982,6 +16982,7 @@ var source = (() => {
       };
       const $2 = await this.fetchCheerio(request);
       const manga = await this.parser.parseHomeSection($2, this);
+      throw new Error(`${manga.length}`);
       metadata = manga.length >= 10 ? { page: page + 1 } : void 0;
       return {
         items: manga,
