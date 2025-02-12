fbbadaa8f502b90e4899b1b65a53bb7e160fcc6d
kymotsujason <kymotsujason@gmail.com>
2025-02-12 05:46:30 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@a913344842a0005ce8bc370391f98008bb2cbfe7 🚀

--- 0.9/MangaFire/index.js
+++ 0.9/MangaFire/index.js
@@ -24802,6 +24802,7 @@ var source = (() => {
           method: "GET"
         };
       }
+      throw new Error(JSON.stringify(request));
       const langSearchFilter = getLanguageSearchFilter() ?? false;
       const langs = getLanguages() ?? MFLanguages.getDefault();
       const $3 = await this.fetchCheerio(request);
