d7fe7dcab0a9735e3f79f218d55ba43d14e7c775
kymotsujason <kymotsujason@gmail.com>
2025-02-13 08:53:14 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@c28c39157784228f37f3a9085271d918cd2f42e1 🚀

--- 0.9/WeebCentral/index.js
+++ 0.9/WeebCentral/index.js
@@ -17256,7 +17256,9 @@ var source = (() => {
     async fetchCheerio(request) {
       const [response, data2] = await Application.scheduleRequest(request);
       this.checkCloudflareStatus(response.status);
-      return load(Application.arrayBufferToUTF8String(data2));
+      return load(Application.arrayBufferToUTF8String(data2), {
+        xml: true
+      });
     }
     checkCloudflareStatus(status) {
       if (status == 503 || status == 403) {
