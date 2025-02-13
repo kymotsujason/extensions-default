3eb0f325cf603db5c93f9cdb03ecf22217e31d79
kymotsujason <kymotsujason@gmail.com>
2025-02-13 11:44:15 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@f476067b7f2054d9c84c2f2033de035d028285f8 🚀

--- 0.9/WeebCentral/index.js
+++ 0.9/WeebCentral/index.js
@@ -17111,7 +17111,11 @@ var source = (() => {
         url: `${WEEBCENTRAL_DOMAIN}/series/${sourceManga.mangaId}/full-chapter-list`,
         method: "GET"
       };
+      var start = (/* @__PURE__ */ new Date()).getTime();
       const $2 = await this.fetchCheerio(request);
+      var end2 = (/* @__PURE__ */ new Date()).getTime();
+      var time = end2 - start;
+      throw new Error(`Fetch chapter details took ${time}ms`);
       return parseChapterList($2, sourceManga);
     }
     async getChapterDetails(chapter) {
@@ -17256,7 +17260,9 @@ var source = (() => {
     async fetchCheerio(request) {
       const [response, data2] = await Application.scheduleRequest(request);
       this.checkCloudflareStatus(response.status);
-      return load(Application.arrayBufferToUTF8String(data2), {
+      const decoder = new TextDecoder("utf-8");
+      const htmlString = decoder.decode(data2);
+      return load(htmlString, {
         xml: {
           xmlMode: false,
           decodeEntities: false
