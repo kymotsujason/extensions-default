51473cf3b1746df965695f08c890f15513be6a88
kymotsujason <kymotsujason@gmail.com>
2025-02-13 10:17:53 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@e39ec5e933a9f4148886f1467ea785ce200c6b24 🚀

--- 0.9/BatoTo/index.js
+++ 0.9/BatoTo/index.js
@@ -22075,13 +22075,13 @@ var source = (() => {
     return result;
   }
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/index.js
+  // node_modules/htmlparser2/lib/esm/index.js
   init_buffer();
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/Parser.js
+  // node_modules/htmlparser2/lib/esm/Parser.js
   init_buffer();
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/Tokenizer.js
+  // node_modules/htmlparser2/lib/esm/Tokenizer.js
   init_buffer();
   var CharCodes2;
   (function(CharCodes3) {
@@ -22769,7 +22769,7 @@ var source = (() => {
     }
   };
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/Parser.js
+  // node_modules/htmlparser2/lib/esm/Parser.js
   var formTags = /* @__PURE__ */ new Set([
     "input",
     "option",
@@ -23217,7 +23217,7 @@ var source = (() => {
     }
   };
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/index.js
+  // node_modules/htmlparser2/lib/esm/index.js
   function parseDocument(data2, options) {
     const handler = new DomHandler(void 0, options);
     new Parser2(handler, options).end(data2);
--- 0.9/Manganato/index.js
+++ 0.9/Manganato/index.js
@@ -16075,13 +16075,13 @@ var source = (() => {
     return result;
   }
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/index.js
+  // node_modules/htmlparser2/lib/esm/index.js
   init_buffer();
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/Parser.js
+  // node_modules/htmlparser2/lib/esm/Parser.js
   init_buffer();
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/Tokenizer.js
+  // node_modules/htmlparser2/lib/esm/Tokenizer.js
   init_buffer();
   var CharCodes2;
   (function(CharCodes3) {
@@ -16769,7 +16769,7 @@ var source = (() => {
     }
   };
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/Parser.js
+  // node_modules/htmlparser2/lib/esm/Parser.js
   var formTags = /* @__PURE__ */ new Set([
     "input",
     "option",
@@ -17217,7 +17217,7 @@ var source = (() => {
     }
   };
 
-  // node_modules/cheerio/node_modules/htmlparser2/lib/esm/index.js
+  // node_modules/htmlparser2/lib/esm/index.js
   function parseDocument(data2, options) {
     const handler = new DomHandler(void 0, options);
     new Parser2(handler, options).end(data2);
@@ -17466,6 +17466,7 @@ var source = (() => {
     }
     async fetchCheerio(request) {
       const [_, data2] = await Application.scheduleRequest(request);
+      return load(Application.arrayBufferToUTF8String(data2));
       return load(Application.arrayBufferToUTF8String(data2), {
         xml: {
           xmlMode: false
