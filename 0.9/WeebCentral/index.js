3e190439d5a5914a035b60286b46c438ec2c3021
kymotsujason <kymotsujason@gmail.com>
2025-02-16 10:38:20 +0000

    Deploying to gh-pages from @ kymotsujason/extensions-default@ceeff7875cc55720466f76d68b3f090f50fb844c 🚀

--- 0.9/WeebCentral/index.js
+++ 0.9/WeebCentral/index.js
@@ -17230,10 +17230,8 @@ var source = (() => {
         searchParams.concat(
           `${included}${excluded}&limit=${LIMIT}&offset=${offset}`
         );
+        throw new Error(searchParams);
       }
-      throw new Error(
-        `${WEEBCENTRAL_DOMAIN}/search/data?sort=Best+Match&order=Ascending&display_mode=Full+Display${searchParams}`
-      );
       const request = {
         url: `${WEEBCENTRAL_DOMAIN}/search/data?sort=Best+Match&order=Ascending&display_mode=Full+Display${searchParams}`,
         method: "GET"
